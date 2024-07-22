edbd466028e3f96b9068a1486b8f74a9f8c1e831
quangthao2002 <quangthao2212002@gmail.com>
2024-07-22 21:40:17 +0700

    add updateFood&paginationFood

--- Food-BE/controllers/foodController.js
+++ Food-BE/controllers/foodController.js
@@ -20,11 +20,31 @@ const addFood = async (req, res) => {
     res.json({ result: "Error", message: error.message });
   }
 };
-//  all food list
-const allFood = async (req, res) => {
+
+// update food item
+const updateFood = async (req, res) => {
+  const { id } = req.params;
+  const food = await foodModel.find({ _id: id });
+
+  let image_filename = food.image; // nếu không thay đổi sử dụng lại image cũ
+  if (req.file) {
+    fs.unlinkSync(`uploads/${food.image}`);
+    image_filename = req.file.filename;
+  }
   try {
-    const foods = await foodModel.find();
-    res.json({ result: "Success", data: foods });
+    const food = await foodModel.findOne({ _id: id });
+    fs.unlinkSync(`uploads/${food.image}`);
+    await foodModel.findOneAndUpdate(
+      { _id: id },
+      {
+        name: req.body.name,
+        description: req.body.description,
+        category: req.body.category,
+        price: req.body.price,
+        image: image_filename,
+      }
+    );
+    res.json({ result: "Success", message: "Food Updated" });
   } catch (error) {
     console.log(error);
     res.json({ result: "Error", message: error.message });
@@ -43,4 +63,57 @@ const removeFood = async (req, res) => {
     res.json({ result: "Error", message: error.message });
   }
 };
-export { addFood, allFood, removeFood };
+
+// get food by id
+const getFood = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const food = await foodModel.findById(id).exec();
+    res.json({ result: "Success", data: food });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ result: "Error", message: error.message });
+  }
+};
+
+//  all food list
+const allFood = async (req, res) => {
+  try {
+    const foods = await foodModel.find();
+    res.json({ result: "Success", data: foods });
+  } catch (error) {
+    console.log(error);
+    res.json({ result: "Error", message: error.message });
+  }
+};
+// food pagination
+
+const paginationFood = async (req, res) => {
+  const page = req.query.page || 1;
+  const limit = req.query.limit || 10;
+  const startIndex = (page - 1) * limit;
+  const result = {};
+
+  try {
+    const totalItems = await foodModel.countDocuments().exec();
+    const totalPages = Math.ceil(totalItems / limit);
+
+    const foods = await foodModel.find().limit(limit).skip(startIndex).exec();
+    // page = 4, limit =10, startIndex = 30
+    result.data = foods;
+    result.totalPages = totalPages;
+    result.currentPage = page;
+
+    if (page < totalPages) {
+      result.nextPage = Number(page) + 1;
+    }
+    if (page > 1) {
+      result.prevPage = page - 1;
+    }
+    res.json({ result: "Success", data: result });
+  } catch (error) {
+    console.log(error);
+    res.json({ result: "Error", message: error.message });
+  }
+};
+export { addFood, allFood, removeFood, paginationFood, updateFood, getFood };
--- Food-BE/routes/foodRoute.js
+++ Food-BE/routes/foodRoute.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { addFood, allFood, removeFood } from "../controllers/foodController.js";
+import {
+  addFood,
+  allFood,
+  getFood,
+  paginationFood,
+  removeFood,
+  updateFood,
+} from "../controllers/foodController.js";
 import multer from "multer";
 const foodRouter = express.Router();
 // Image Storage Engine
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads");
+    cb(null, "uploads"); // set  thu muc luu images
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`);
@@ -13,6 +20,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list/pagination", paginationFood);
 foodRouter.get("/list-food", allFood);
 foodRouter.delete("/remove/:id", removeFood);
+foodRouter.put("/update/:id", upload.single("image"), updateFood);
+foodRouter.get("/getfood/:id", getFood);
 export default foodRouter;
